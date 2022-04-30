97c398039f5fb207bc254eadd94b70f689faaf6b
unknown <lujugames13@gmail.com>
2022-04-30 19:09:26 -0300

    Backend

--- BackEnd/Domain/Contacts/contacts.contoller.js
+++ BackEnd/Domain/Contacts/contacts.contoller.js
@@ -3,9 +3,10 @@ const { errorHandler } = require("../../helpers/errorHandler");
 
 const addContactList = async (req, res) => {
   try {
-    const { name, lastName, mail, phone, message } = req.body;
+    const { id, name, lastName, mail, phone, message } = req.body;
 
     const newMessage = await Contacts.create({
+      id: id,
       first_name: name,
       last_name: lastName,
       email: mail,
